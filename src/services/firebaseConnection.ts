dc924d7cf5e534a52cf76bd6b01bbc7bcb33577c
Eduardo Soares <128731192+eduardossoares@users.noreply.github.com>
2024-10-08 19:56:32 -0300

    Update firebaseConnection.ts

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHBbU4i9oZLLpmbNads8IRqkF8K1MmskE",
-  authDomain: "webcarrps.firebaseapp.com",
-  projectId: "webcarrps",
-  storageBucket: "webcarrps.appspot.com",
-  messagingSenderId: "1049027392898",
-  appId: "1:1049027392898:web:6a729d391538eab5b34b9b",
-  measurementId: "G-W9C54GJVTK"
+  // Atualizar .ENV
 };
 
 const app = initializeApp(firebaseConfig);
